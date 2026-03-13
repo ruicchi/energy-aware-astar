b44ba7b93cc485479cd60d977fe3902665c2b693
Luis Capiral <ihategmpls@gmail.com>
2026-03-14 00:39:51 +0800

    refactor: viewport and mouse click hooks

--- src/grid/index.ts
+++ src/grid/index.ts
@@ -1,2 +1,6 @@
 export { default as Box } from '@mui/material/Box';
+
 export { useEffect, useMemo, useState } from 'react';
+
+export { useViewport } from '../hooks/useViewport'; 
+export { useGridMouseClicks } from '../hooks/useGridMouseClicks';
