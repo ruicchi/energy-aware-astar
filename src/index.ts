756b25eb01fe1bda5518b9902d63dfe3cf5d875e
ruicchi <ihategmpls@gmail.com>
2026-03-29 22:41:06 +0800

    chore: clean code

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export { FloatingMenu } from './game/FloatingMenu';
 export { useViewport } from './hooks/useViewport';
 export { useGridMouseClicks } from './hooks/useGridMouseClicks';
 export { toggleWallState, clearWalls } from './utils/wallUtils';
-export { runAStarManhattan as runAStar } from './algorithms/astar/astarManhattan';
+export { runAStarManhattan } from './algorithms/astar/astarManhattan';
 export { runAStarEnergyAware } from './algorithms/astar/astarEnergyAware';
