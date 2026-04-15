847356779aa5c50a35afa6c00b33b5b09fb0a3dd
ruicchi <ihategmpls@gmail.com>
2026-04-16 02:00:31 +0800

    chore: clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import GameGrid from './game/game';
-import './App.css';
+import GameGrid from "./game/game";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <GameGrid />
     </div>
   );
