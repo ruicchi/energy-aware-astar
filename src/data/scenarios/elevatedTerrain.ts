9b7b34957fdbf8f28dadb49e1f034705fb0204f3
ruicchi <ihategmpls@gmail.com>
2026-05-16 12:29:12 +0800

    test: add robot physics to elevated terrain scenario

---
+++ src/data/scenarios/elevatedTerrain.ts
@@ -0,0 +1,25 @@
+import { type Scenario } from "../../types"
+
+export const elevatedTerrain: Scenario = {
+  rows: 20,
+  cols: 20,
+  robotNode: "2-2",
+  destinationNode: "18-18",
+  initialHeading: "DOWN_RIGHT",
+  wallNodes: new Set(),
+  terrainFactors: new Map(),
+  elevations: new Map([
+    ["5-5", 1], ["5-6", 1], ["5-7", 1],
+    ["6-5", 2], ["6-6", 2], ["6-7", 2],
+    ["7-5", 3], ["7-6", 3], ["7-7", 3],
+  ]),
+  climbingFactor: 10,
+  turnPenalty: 2,
+  maxTraversableSlope: 15,
+  robotPhysics: {
+    trackWidth: 0.6,
+    wheelBase: 0.8,
+    comHeight: 0.4,
+    stabilityMargin: 0.05
+  }
+}
